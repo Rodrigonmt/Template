62e6bd63d3f54ecc82c4361732850a608aa18599
Rodrigo <seu_email@exemplo.com>
2026-09-15 23:05:07 -0300

    Aula4_metade

---
+++ Template.Data/Context/TemplateContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Template.Data.Mappings;
+using Template.Domain.Entities;
+
+namespace Template.Data.Context
+{
+    public class TemplateContext : DbContext
+    {
+        public TemplateContext(DbContextOptions<TemplateContext> options)
+            : base(options) { }
+
+        #region
+        public DbSet<User> Users { get; set; }
+        #endregion
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Apply entity configurations
+            modelBuilder.ApplyConfiguration(new UserMap());
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
