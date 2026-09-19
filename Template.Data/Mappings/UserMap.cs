1baf925cbbe918995094d61e554f3f292cf4a27f
Rodrigo <seu_email@exemplo.com>
2026-09-19 00:42:17 -0300

    Comentando codigos

--- Template.Data/Mappings/UserMap.cs
+++ Template.Data/Mappings/UserMap.cs
@@ -7,12 +7,19 @@ using Template.Domain.Entities;
 
 namespace Template.Data.Mappings
 {
+    // Declara a classe de mapeamento para a entidade User implementando a interface Fluent API do EF Core
     public class UserMap : IEntityTypeConfiguration<User>
     {
+        // Método exigido pela interface para aplicar as regras de esquema do banco de dados sobre a entidade
         public void Configure(EntityTypeBuilder<User> builder)
         {
+            // Configura a propriedade 'Id' para ser gerada como campo obrigatório (NOT NULL)
             builder.Property(x => x.Id).IsRequired();
+
+            // Configura a propriedade 'Name' como obrigatória (NOT NULL) e limita a coluna a no máximo 100 caracteres (nvarchar(100))
             builder.Property(x => x.Name).IsRequired().HasMaxLength(100);
+
+            // Configura a propriedade 'Email' como obrigatória (NOT NULL) e limita a coluna a no máximo 100 caracteres (nvarchar(100))
             builder.Property(x => x.Email).IsRequired().HasMaxLength(100);
         }
     }
--- Template.Server/Program.cs
+++ Template.Server/Program.cs
@@ -1,25 +1,27 @@
 using Microsoft.EntityFrameworkCore;
+
 using Template.Data.Context;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Substituição direta da linha destacada:
+// Configura e regista o DbContext (TemplateContext) no contêiner de dependências
+// Define o provedor SQL Server utilizando a ConnectionString "DefaultConnection" definida no appsettings.json
+// Ativa a exibição de dados sensíveis (parâmetros de queries SQL) nos logs de depuração
 builder.Services.AddDbContext<TemplateContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
            .EnableSensitiveDataLogging());
 
-// Add services to the container.
-
+// Adiciona os serviços necessários para suportar o uso de Controllers da API
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
 app.UseDefaultFiles();
+
 app.MapStaticAssets();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
